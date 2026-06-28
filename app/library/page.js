2459636cb55336990c13cc6f2cf2011209a1c3cc
boat-s-1 <marron.toyohashi@gmail.com>
2026-06-28 23:55:01 +0900

    Update page.js

--- app/library/page.js
+++ app/library/page.js
@@ -50,7 +50,7 @@ const sections = [
         href: "/library/kiina-seminar",
         className: "yellowBook",
         cover: "/6716D6BF-80F0-415A-BC81-0270FB704655.png",
-        inside: "/2906B318-96E8-4CE9-AA58-2591BC5F6007.png",
+        inside: "/4A1DFE79-8D3F-4D28-BFCA-E97C36B752D2.png",
       },
     ],
   },
