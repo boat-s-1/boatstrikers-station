ac1a963375a87b8849f44607acda5857f48d9b23
boat-s-1 <marron.toyohashi@gmail.com>
2026-07-01 03:29:12 +0900

    Create page.js

---
+++ app/bsc2/play/chapter1/page.js
@@ -0,0 +1,15 @@
+import ChatEngine from "../../components/ChatEngine";
+import chapter1 from "../../story/chapter1";
+
+export default function Chapter1Page() {
+  return (
+    <ChatEngine
+      storyId="chapter1-lesson1"
+      title="BOAT STRIKERS CHALLENGE"
+      chapter="Chapter 1"
+      story={chapter1}
+      rewardPoint={20}
+      badge="競艇入門"
+    />
+  );
+}
