943b7bed729ee3340d7c48299d0313a4908fec21
boat-s-1 <marron.toyohashi@gmail.com>
2026-07-01 01:27:21 +0900

    Create characters.js

---
+++ app/bsc2/data/characters.js
@@ -0,0 +1,34 @@
+const characters = {
+  ichika: {
+    name: "一果",
+    color: "#ff4f93",
+    images: {
+      normal: "/characters/ichika-talk.png",
+      happy: "/characters/ichika-talk.png",
+      thinking: "/characters/ichika-talk.png",
+      surprise: "/characters/ichika-talk.png",
+    },
+  },
+  hatsune: {
+    name: "初音",
+    color: "#8a55e6",
+    images: {
+      normal: "/characters/hatsune-talk.png",
+      happy: "/characters/hatsune-talk.png",
+      thinking: "/characters/hatsune-talk.png",
+      surprise: "/characters/hatsune-talk.png",
+    },
+  },
+  kiina: {
+    name: "キイナ",
+    color: "#f6a800",
+    images: {
+      normal: "/characters/kiina-talk.png",
+      happy: "/characters/kiina-talk.png",
+      thinking: "/characters/kiina-talk.png",
+      surprise: "/characters/kiina-talk.png",
+    },
+  },
+};
+
+export default characters;
