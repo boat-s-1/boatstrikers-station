e122ea0430f79587e09944c0b4dbf97b5e7742f4
boat-s-1 <marron.toyohashi@gmail.com>
2026-07-01 15:06:01 +0900

    Update page.js

--- app/bsc2/page.js
+++ app/bsc2/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import BscStatus from "./components/BscStatus";
+import DailyVote from "./components/DailyVote";
 
 export default function BSC2MenuPage() {
   const [cleared, setCleared] = useState([]);
@@ -33,7 +34,7 @@ export default function BSC2MenuPage() {
       </section>
 
       <BscStatus />
-
+　　　　<DailyVote/>
       <section className="bscMenuList">
         {continueData && (
           <a href={continueData.url} className="bscMenuButton continue">
