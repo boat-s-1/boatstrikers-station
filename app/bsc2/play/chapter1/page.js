8249cdbe90078a5bdbc8acb2fbdd04a0dfa11dbc
boat-s-1 <marron.toyohashi@gmail.com>
2026-07-04 20:10:11 +0900

    Update page.js

--- app/bsc2/play/chapter1/page.js
+++ app/bsc2/play/chapter1/page.js
@@ -1,17 +1,6 @@
-import ChatEngine from "../../components/ChatEngine";
+import ChapterRpgEngine from "../../components/ChapterRpgEngine";
 import chapter1 from "../../story/chapter1";
 
 export default function Chapter1Page() {
-  return (
-    <ChatEngine
-      storyId={chapter1.id}
-      title="BOAT STRIKERS CHALLENGE"
-      chapter={chapter1.title}
-      subtitle={chapter1.subtitle}
-      story={chapter1.steps}
-      rewardPoint={chapter1.reward.point}
-      badge={chapter1.reward.badge.name}
-      reward={chapter1.reward}
-    />
-  );
+  return <ChapterRpgEngine chapterData={chapter1} />;
 }
