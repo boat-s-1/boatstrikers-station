929b30609f4fea7faf8d251bad671883fd6bde59
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-04 03:17:59 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import "./globals.css";
 import PublicBottomNav from "./PublicBottomNav";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
   metadataBase: new URL("https://www.boat-strike.online"),
-
   title: {
     default: "BoatStrikers｜ボートレースをもっと楽しく、分かりやすく",
     template: "%s｜BoatStrikers",
@@ -20,6 +20,15 @@ export default function RootLayout({ children }) {
         {children}
         <PublicBottomNav />
       </body>
+    </html>
+  );
+}
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+
+      <GoogleAnalytics gaId="G-DXF6FFZ574" />
     </html>
   );
 }
