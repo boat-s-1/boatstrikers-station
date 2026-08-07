1413210373d68daee383b9c63d13da23b28a570d
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-07 23:42:22 +0900

    Update page.js

--- app/disclaimer/page.js
+++ app/disclaimer/page.js
@@ -318,7 +318,7 @@ export default function DisclaimerPage() {
         <a href="/ichika-sensei">教えて！一果センセー</a>
         <a href="/comic">ふなけん研究部</a>
         <a href="/radio">ボート・ナイト・ニッポン</a>
-        <a href="/program">番組表</a>
+        <a href="/schedule">番組表</a>
       </div>
 
       <div className="footerLinkGroup">
