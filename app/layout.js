273b97db46d1b30b21b341a24e805760931b33ad
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-23 04:01:13 +0900

    fix(ichika): mount safe AI banner renderer

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import PublicSiteHeader from "./PublicSiteHeader";
 import CharacterAiRoomPanel from "./components/CharacterAiRoomPanel";
 import CharacterShortsShelf from "./components/CharacterShortsShelf";
 import IchikaRoomTheme from "./components/IchikaRoomTheme";
+import IchikaSafeBanner from "./components/IchikaSafeBanner";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         <CharacterAiRoomPanel />
         <CharacterShortsShelf />
         <IchikaRoomTheme />
+        <IchikaSafeBanner />
         <PublicBottomNav />
         <GoogleAnalytics gaId="G-DXF6FFZ574" />
       </body>
