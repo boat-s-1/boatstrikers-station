7da999b96d991efe39e696c7780fd3f70634aa63
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-23 13:11:18 +0900

    style(header): load stronger logo sizing

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import "./public-header-glass.css";
+import "./public-header-logo-boost.css";
 import PublicBottomNav from "./PublicBottomNav";
 import PublicSiteHeader from "./PublicSiteHeader";
 import CharacterAiRoomPanel from "./components/CharacterAiRoomPanel";
