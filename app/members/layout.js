269e779d60970a2b8eec4d6db9ca45b2842ff322
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-27 01:27:17 +0900

    Mount Hatsune member notification activation

---
+++ app/members/layout.js
@@ -0,0 +1,5 @@
+import HatsuneInnerBreakActivation from "./HatsuneInnerBreakActivation";
+
+export default function MembersLayout({children}){
+  return <>{children}<HatsuneInnerBreakActivation/></>;
+}
