7ffb6ca70bcaf68e044ab8ca684ab44af8f1f53f
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-27 07:25:27 +0900

    Force deploy unified 24 stadium guide template

--- app/library/stadium/[place]/layout.js
+++ app/library/stadium/[place]/layout.js
@@ -1,6 +1,7 @@
 import StadiumGuideQuickNav from './StadiumGuideQuickNav';
 import './unifiedStadiumGuide.css';
 
+// Shared shell for all 24 BoatStrikers stadium strategy pages.
 export default function StadiumGuideLayout({ children }) {
   return (
     <div className="stadiumUnified24">
