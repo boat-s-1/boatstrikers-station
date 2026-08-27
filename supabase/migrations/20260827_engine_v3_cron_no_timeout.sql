73edd470062c84fd29f6172044cdd880582cf742
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-27 17:38:23 +0900

    Disable timeout for weekly stadium aggregation cron

---
+++ supabase/migrations/20260827_engine_v3_cron_no_timeout.sql
@@ -0,0 +1,25 @@
+do $$
+declare
+  v_jobid bigint;
+begin
+  select jobid
+    into v_jobid
+  from cron.job
+  where jobname = 'boatstrikers-engine-v3-weekly'
+  limit 1;
+
+  if v_jobid is not null then
+    perform cron.unschedule(v_jobid);
+  end if;
+
+  perform cron.schedule(
+    'boatstrikers-engine-v3-weekly',
+    '0 19 * * 0',
+    $cron$
+      set statement_timeout = 0;
+      select *
+      from public.bs_engine_v3_refresh_all(current_date);
+    $cron$
+  );
+end
+$$;
