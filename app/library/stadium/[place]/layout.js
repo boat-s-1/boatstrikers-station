1732324d69ce0b8cc12aec508ace4b48e47cfa61
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-27 16:06:59 +0900

    Trigger production deploy for unified 24 stadium guide

--- app/library/stadium/[place]/layout.js
+++ app/library/stadium/[place]/layout.js
@@ -2,6 +2,7 @@ import StadiumGuideQuickNav from './StadiumGuideQuickNav';
 import './unifiedStadiumGuide.css';
 
 // Shared shell for all 24 BoatStrikers stadium strategy pages.
+// Production refresh: unified navigation and mobile layout.
 export default function StadiumGuideLayout({ children }) {
   return (
     <div className="stadiumUnified24">
