2da3bffe47cfec40b953fa0bf385f7601fc0d16b
boat-s-1 <marron.toyohashi@gmail.com>
2026-08-28 05:09:52 +0900

    feat: mount newspaper prompt assistant

--- app/admin/newspaper/page.js
+++ app/admin/newspaper/page.js
@@ -1,9 +1,15 @@
 import NewspaperAdminClient from "./NewspaperAdminClient";
+import NewspaperPromptAssistant from "./NewspaperPromptAssistant";
 
 export const metadata = {
   title: "新聞作成 | BoatStrikers CMS",
 };
 
 export default function NewspaperAdminPage() {
-  return <NewspaperAdminClient />;
+  return (
+    <>
+      <NewspaperAdminClient />
+      <NewspaperPromptAssistant />
+    </>
+  );
 }
