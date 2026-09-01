f8d5de92d17789badf2c45325dccaa225c9f435e
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-02 02:10:47 +0900

    Noindex admin pages

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -2,6 +2,10 @@ import AdminTopBackLink from "./AdminTopBackLink";
 import AdminHatsuneNewsShortcut from "./AdminHatsuneNewsShortcut";
 import "./adminLayout.css";
 
+export const metadata = {
+  robots: { index: false, follow: false, noarchive: true },
+};
+
 export default function AdminLayout({ children }) {
   return (
     <>
