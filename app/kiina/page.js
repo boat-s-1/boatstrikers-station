c104e2c32f87819e8dc922af8e684df42ef67a0d
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-06 07:46:47 +0900

    Remove legacy footer nav from Kiina page

--- app/kiina/page.js
+++ app/kiina/page.js
@@ -592,14 +592,6 @@ export default async function KiinaPage() {
           />
         </a>
       </section>
-
-      <nav className="bottomNav">
-        <a href="/">ホーム</a>
-        <a href="/ichika">一果</a>
-        <a href="/hatsune">初音</a>
-        <a href="/kiina">キイナ</a>
-        <a href="/library">図書館</a>
-      </nav>
     </main>
   );
 }
