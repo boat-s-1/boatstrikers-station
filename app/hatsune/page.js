664ab5af885c12674084f2418b17d8cc25050f87
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-06 09:05:33 +0900

    Remove legacy bottom nav from Hatsune page

--- app/hatsune/page.js
+++ app/hatsune/page.js
@@ -198,10 +198,6 @@ export default async function HatsunePage() {
           <img src="/90F7EE0F-5EE8-4E71-BF86-D254BB00750D.png" alt="公式LINE登録" className="lineBannerImage" />
         </a>
       </section>
-
-      <nav className="bottomNav">
-        <a href="/">ホーム</a><a href="/ichika">一果</a><a href="/hatsune">初音</a><a href="/kiina">キイナ</a><a href="/library">図書館</a>
-      </nav>
     </main>
   );
 }
