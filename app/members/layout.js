94eb6423ce6385f168c0ed9799758dbe3703cffc
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-14 23:40:12 +0900

    Show member usage dashboard on members page

--- app/members/layout.js
+++ app/members/layout.js
@@ -1,10 +1,11 @@
 import KiinaTheoryLabelBridge from "./KiinaTheoryLabelBridge";
 import MemberChannelBridge from "./MemberChannelBridge";
+import MemberUsageDashboard from "./MemberUsageDashboard";
 
 export const metadata = {
   robots: { index: false, follow: false, noarchive: true },
 };
 
 export default function MembersLayout({children}){
-  return <>{children}<KiinaTheoryLabelBridge /><MemberChannelBridge /></>;
+  return <>{children}<MemberUsageDashboard /><KiinaTheoryLabelBridge /><MemberChannelBridge /></>;
 }
