f6892fc331d7fbd4f0fb9218298c03648b751c7f
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-24 06:05:24 +0900

    Apply Ichika page polish globally

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ import MemberEmailConfirmationHelper from "./components/MemberEmailConfirmationH
 import MemberModeQueryBridge from "./components/MemberModeQueryBridge";
 import BoatAnalyticsTracker from "./components/BoatAnalyticsTracker";
 import LatestCharacterPopup from "./components/LatestCharacterPopup";
+import IchikaPagePolish from "./components/IchikaPagePolish";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
@@ -51,6 +52,7 @@ export default function RootLayout({ children }) {
         {children}
         <BetaMemberPlacementBanner />
         <CharacterAiRoomPanel />
+        <IchikaPagePolish />
         <CharacterShortsShelf />
         <CharacterPerformancePortal />
         <RouteScopedVisuals />
