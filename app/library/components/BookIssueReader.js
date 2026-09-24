f13fa7814c3db6c842dc8e6bbd88b363685a109b
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-24 23:53:31 +0900

    fix: use dedicated comic viewer for Ichika books

--- app/library/components/BookIssueReader.js
+++ app/library/components/BookIssueReader.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import MagazineSwipeViewer from "./MagazineSwipeViewer";
+import BookSwipeViewer from "./BookSwipeViewer";
 import BookArticle from "./BookArticle";
 import styles from "./BookIssueReader.module.css";
 
 export default function BookIssueReader({ series, issue }) {
   return (
     <>
-      <MagazineSwipeViewer magazine={series} issue={issue} />
+      <BookSwipeViewer series={series} issue={issue} />
       <BookArticle article={issue.article} />
       <nav className={styles.bookNav} aria-label="本のナビゲーション">
-        <Link href={`/${series.slug}`}>← 本の一覧へ戻る</Link>
+        <Link href={series.basePath}>← 本の一覧へ戻る</Link>
         <Link href="/ichika">一果の部屋へ</Link>
       </nav>
     </>
