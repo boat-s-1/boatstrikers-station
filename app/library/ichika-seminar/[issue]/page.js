e2aea750549b5cb3952f0935a845c1cc852edc93
boat-s-1 <marron.toyohashi@gmail.com>
2026-09-26 23:07:28 +0900

    Correct Ichika seminar posting path

--- app/library/ichika-seminar/[issue]/page.js
+++ app/library/ichika-seminar/[issue]/page.js
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import MagazineSwipeViewer from "../../components/MagazineSwipeViewer";
+import BookArticle from "../../components/BookArticle";
 import { seminarMagazines } from "../../../../lib/seminarMagazines";
 import { getSeminarIssue } from "../../../../lib/seminarMagazineLibrary";
 export const dynamic = "force-dynamic";
-export default async function IssuePage({ params }) { const { issue: issueId }=await params; const issue=await getSeminarIssue("ichika",issueId); if(!issue)notFound(); return <MagazineSwipeViewer magazine={seminarMagazines.ichika} issue={issue}/>; }
+export default async function IssuePage({ params }) { const { issue: issueId }=await params; const issue=await getSeminarIssue("ichika",issueId); if(!issue)notFound(); return <><MagazineSwipeViewer magazine={seminarMagazines.ichika} issue={issue}/><BookArticle article={issue.article}/></>; }
